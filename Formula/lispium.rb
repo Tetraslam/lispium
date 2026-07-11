087ac4373c187d39e61d0405e34f3d08a42341d5
Shresht Bhowmick <bhowmickshresht@gmail.com>
2026-07-10 18:04:04 -0700

    Update package manifest checksums for v0.7.0 release assets

--- Formula/lispium.rb
+++ Formula/lispium.rb
@@ -7,22 +7,22 @@ class Lispium < Formula
   on_macos do
     on_arm do
       url "https://github.com/Tetraslam/lispium/releases/download/v0.7.0/lispium-macos-aarch64.tar.gz"
-      sha256 "b802d6959bcb3757ab59d1c92e4853d2c7a81f91623d69f590476f920c70d5a8"
+      sha256 "5432521b213b6bb2cd1aa5c633f9cee19acee9180f28117dfb53288cb1fcb8f7"
     end
     on_intel do
       url "https://github.com/Tetraslam/lispium/releases/download/v0.7.0/lispium-macos-x86_64.tar.gz"
-      sha256 "017233c12eefbeb5c3effe928d7843a19164fe9a907c491b2be99e3c5ae47c32"
+      sha256 "2da8093182a763187616e11f4f96028113a96cf3032e70159f29f0e7c716b601"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Tetraslam/lispium/releases/download/v0.7.0/lispium-linux-aarch64.tar.gz"
-      sha256 "a001814b696e1c88ae992b20970c0f17b403bbe235d8050b09fcecb3543ddd55"
+      sha256 "afd9b702f84607ad744ea7a0b4b83c6fce2b1c5b17974f1f2b22d159d0ae7b07"
     end
     on_intel do
       url "https://github.com/Tetraslam/lispium/releases/download/v0.7.0/lispium-linux-x86_64.tar.gz"
-      sha256 "32aa3c2240369094fca586439d8f24e09801fbcba0f1d852e880b4d38db91949"
+      sha256 "3b24d06193487c36413d502ca7bc4d192c4d6647c139e1c840ae4f7feb374ee6"
     end
   end
 
