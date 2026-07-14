8818b10a2b33fe4a3e9cac7a310b247f88fee79a
Shresht Bhowmick <bhowmickshresht@gmail.com>
2026-07-13 18:28:24 -0700

    Update package manifest checksums for v0.14.0 release assets

--- Formula/lispium.rb
+++ Formula/lispium.rb
@@ -1,28 +1,28 @@
 class Lispium < Formula
   desc "Symbolic Computer Algebra System written in Zig"
   homepage "https://github.com/Tetraslam/lispium"
-  version "0.13.0"
+  version "0.14.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Tetraslam/lispium/releases/download/v0.13.0/lispium-macos-aarch64.tar.gz"
-      sha256 "5d6ae6d8e418afddf3e2546110b1d68b68872a2daca34fc10473ef0a2a691d4d"
+      url "https://github.com/Tetraslam/lispium/releases/download/v0.14.0/lispium-macos-aarch64.tar.gz"
+      sha256 "e20f97ef5efda92d59c1f6c9db36aba78289dbed948230a0fe15535373fb8d4f"
     end
     on_intel do
-      url "https://github.com/Tetraslam/lispium/releases/download/v0.13.0/lispium-macos-x86_64.tar.gz"
-      sha256 "71632e9ff0192351fd757e1e9dff75bd4788e14e4575b91e93295e206186ca9e"
+      url "https://github.com/Tetraslam/lispium/releases/download/v0.14.0/lispium-macos-x86_64.tar.gz"
+      sha256 "307a8bb668cd0c55338e8e71357397b291bf7247ba89887d8131266e7b34e90c"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Tetraslam/lispium/releases/download/v0.13.0/lispium-linux-aarch64.tar.gz"
-      sha256 "1cfe41eedfded3762defdf0d52cf67bcf7e8c3ee3d538f05c3ae0e169b2ae841"
+      url "https://github.com/Tetraslam/lispium/releases/download/v0.14.0/lispium-linux-aarch64.tar.gz"
+      sha256 "60c5e37f6cb3bfbf219f3ad1db5c009fc9925ab9c0f283eb534ab660604a851e"
     end
     on_intel do
-      url "https://github.com/Tetraslam/lispium/releases/download/v0.13.0/lispium-linux-x86_64.tar.gz"
-      sha256 "0792ef90523305eb0af37609c6e359a212899cdbb1031de7cef0d67ffb774b59"
+      url "https://github.com/Tetraslam/lispium/releases/download/v0.14.0/lispium-linux-x86_64.tar.gz"
+      sha256 "3d711c9f8b13a2d5e0e90f7e36435f3561a815d30ddcdbb6d4614565d4a2887f"
     end
   end
 
